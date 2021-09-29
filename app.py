40442e5d349737534c1b9b08fd1c1cdc3ca79595
HassanYousef1985 <31389144+HassanYousef1985@users.noreply.github.com>
2021-09-29 16:47:37 +0200

    first commit

--- app.py
+++ app.py
@@ -491,7 +491,7 @@ def main():
 
 
                             # Read and load the JSON file
-                            json_file = open('<model.json', 'r')
+                            json_file = open('model.json', 'r')
                             loaded_model_json = json_file.read()
                             json_file.close()
 
