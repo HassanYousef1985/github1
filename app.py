be5c9d18f041934be5d502fa285beba203fe44a0
HassanYousef1985 <31389144+HassanYousef1985@users.noreply.github.com>
2021-10-07 11:26:28 +0200

    first commit

--- app.py
+++ app.py
@@ -292,15 +292,9 @@ def main():
         # word_embeddings_clf.save('my_model.h5')
 
         
-        stream = st.file_uploader('TF.Keras model file (.h5py.zip)', type='zip')
-        if stream is not None:
-            myzipfile = zipfile.ZipFile(stream)
-            with tempfile.TemporaryDirectory() as tmp_dir:
-                myzipfile.extractall(tmp_dir)
-                root_folder = myzipfile.namelist()[0] # e.g. "model.h5py"
-                model_dir = os.path.join(tmp_dir, root_folder)
-                #st.info(f'trying to load model from tmp dir {model_dir}...')
-                model = load_model(model_dir)
+        model = load_model('my_model.h5')
+
+
 
            
 
